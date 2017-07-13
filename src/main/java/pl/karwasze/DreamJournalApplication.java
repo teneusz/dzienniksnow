44398e78b8e21fc7173712268899f866d155360f
Teneusz <teneusz@wp.pl>
2017-07-13 16:29:33 +0200

    inital commit

---
+++ src/main/java/pl/karwasze/DreamJournalApplication.java
@@ -0,0 +1,12 @@
+package pl.karwasze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamJournalApplication.class, args);
+	}
+}
