cbe89b1ea1598bf162663ea459c96f3e3a89b8d9
Teneusz <teneusz@wp.pl>
2017-07-13 17:11:18 +0200

    totaly bullshit :D

--- src/main/java/pl/karwasze/DreamJournalApplication.java
+++ src/main/java/pl/karwasze/DreamJournalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DreamJournalApplication {
 
+	//Komentarz testowy sprawdzający jenkinsa
 	public static void main(String[] args) {
 		SpringApplication.run(DreamJournalApplication.class, args);
 	}
